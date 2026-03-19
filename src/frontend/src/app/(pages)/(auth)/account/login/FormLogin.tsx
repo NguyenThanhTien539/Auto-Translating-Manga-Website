63de0e18673f12ef96e9dba5185efb4107358b48
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2026-03-19 17:31:52 +0700

    change login layout

--- src/frontend/src/app/(pages)/(auth)/account/login/FormLogin.tsx
+++ src/frontend/src/app/(pages)/(auth)/account/login/FormLogin.tsx
@@ -1,19 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import JustValidate from "just-validate";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import MyCustomGoogleButton from "@/app/hooks/useGoogle";
 
 const roleRedirectMap: Record<string, string> = {
   0: "/admin/dashboard",
-  1: "/", // tuỳ bạn đặt
-  2: "/", // có thể là "/explore"
+  1: "/",
+  2: "/",
 };
 
 export default function FormLogin() {
   const router = useRouter();
+  const handleSuccessGoogleLogin = async (credentialResponse: any) => {
+    const { credential } = credentialResponse;
+    const dataFinal = { credential: credential, rememberPassword: false };
 
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/account/google-login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(dataFinal),
+          credentials: "include",
+        },
+      );
+      const data = await res.json();
+
+      if (data.code === "error") {
+        toast.error(data.message || "Google login failed");
+      } else if (data.code === "success") {
+        toast.success(data.message || "Login successful!");
+        data.role === "0" ? router.push("/admin/dashboard") : router.push("/");
+      }
+    } catch (error) {
+      toast.error("Failed to connect to server");
+    }
+  };
   useEffect(() => {
     const validate = new JustValidate("#loginForm", { lockForm: false });
 
@@ -21,15 +48,15 @@ export default function FormLogin() {
       .addField(
         "#email",
         [
-          { rule: "required", errorMessage: "Vui lòng nhập email!" },
-          { rule: "email", errorMessage: "Email không đúng định dạng" },
+          { rule: "required", errorMessage: "Email is required!" },
+          { rule: "email", errorMessage: "Invalid email format!" },
         ],
-        { errorContainer: "#emailError" }
+        { errorContainer: "#emailError" },
       )
       .addField(
         "#password",
-        [{ rule: "required", errorMessage: "Vui lòng nhập mật khẩu!" }],
-        { errorContainer: "#passwordError" }
+        [{ rule: "required", errorMessage: "Password is required!" }],
+        { errorContainer: "#passwordError" },
       )
       .onSuccess((event: any) => {
         const email = event.target.email.value;
@@ -37,9 +64,9 @@ export default function FormLogin() {
         const rememberPassword = event.target.rememberPassword.checked;
 
         const dataFinal = {
-          email: email,
-          password: password,
-          rememberPassword: rememberPassword,
+          email,
+          password,
+          rememberPassword,
         };
 
         fetch(`${process.env.NEXT_PUBLIC_API_URL}/account/login`, {
@@ -51,93 +78,121 @@ export default function FormLogin() {
           .then((res) => res.json())
           .then((data) => {
             if (data.code == "error") {
-              toast.error(data.message || "Đăng nhập thất bại!");
+              toast.error(data.message || "Login failed!");
             }
-
             if (data.code == "success") {
-              toast.success("Đăng nhập thành công!");
-
+              toast.success("Login successful!");
               const role = data.role as string;
               const target = roleRedirectMap[role] ?? "/";
-
               router.push(target);
             }
           });
       });
-  }, []);
+  }, [router]);
 
   return (
-    <>
-      <form id="loginForm" action="">
+    <div className="max-w-md mx-auto p-4">
+      <form id="loginForm">
         <div className="flex flex-col gap-4 mt-[25px]">
+          {/* Email */}
           <div>
-            <label className="block font-[500] text-[14px] mb-[5px]">
+            <label
+              htmlFor="email"
+              className="block font-[500] text-[14px] mb-[5px]"
+            >
               Email*
             </label>
             <input
-              type="text"
               id="email"
-              placeholder="Ví dụ: nva@gmail.com"
-              className="border border-gray-500 rounded-lg p-2 w-full  "
+              name="email"
+              type="email"
+              placeholder="e.g. john@example.com"
+              className="border border-gray-400 rounded-lg p-2 w-full focus:outline-blue-500"
             />
-            <div id="emailError" className="text-sm text-red"></div>
+            <div id="emailError" className="text-sm text-red-500 mt-1"></div>
           </div>
+          {/* Password */}
           <div>
-            <label className="block font-[500] text-[14px] mb-[5px]">
-              Mật khẩu*
+            <label
+              htmlFor="password"
+              className="block font-[500] text-[14px] mb-[5px]"
+            >
+              Password*
             </label>
             <input
-              type="password"
               id="password"
+              name="password"
+              type="password"
               placeholder="******"
-              className="border border-gray-[500] rounded-lg p-2 w-full  "
+              className="border border-gray-400 rounded-lg p-2 w-full focus:outline-blue-500"
             />
-            <div id="passwordError" className="text-sm text-red"></div>
+            <div id="passwordError" className="text-sm text-red-500 mt-1"></div>
           </div>
-
+          {/* Remember me & Forgot password */}
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <input
                 id="rememberPassword"
+                name="rememberPassword"
                 type="checkbox"
-                className="w-[20px] h-[20px]"
+                className="w-4 h-4"
               />
-              <label htmlFor="rememberPassword" className="ml-[10px]">
-                Nhớ mật khẩu
+              <label
+                htmlFor="rememberPassword"
+                className="ml-[10px] text-[14px]"
+              >
+                Remember me
               </label>
             </div>
-            <span
-              className="text-blue-500 cursor-pointer"
-              onClick={() => {
-                router.push("/account/forgot-password");
-              }}
-            >
-              Quên mật khẩu
-            </span>
-          </div>
-
-          <div className="text-center mt-[2px]">
             <button
-              className=" bg-blue-500 border border-[#DEDEDE] rounded-lg w-[100px] h-[40px] font-[700] text-[16px] text-white cursor-pointer"
-              type="submit"
+              type="button"
+              className="text-blue-500 font-[600] hover:underline"
+              onClick={() => router.push("/account/forgot-password")}
             >
-              Đăng nhập
+              Recovery password
             </button>
           </div>
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="bg-[#1B6FAB] hover:bg-[#155a8a] transition-colors rounded-lg w-full h-[48px] font-[700] text-[16px] text-white"
+          >
+            Log in
+          </button>
 
-          <div className="text-center text-[14px]">
-            Bạn chưa có tài khoản?
-            <span
-              className="pl-1 text-blue-500 font-[600] cursor-pointer hover:text-blue-700"
-              onClick={() => {
-                router.push("/account/register");
-              }}
+          <div className="flex justify-center w-full">
+            <GoogleOAuthProvider
+              clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}
+              locale="en"
             >
-              Tạo tài khoản
-            </span>
+              <MyCustomGoogleButton onSuccess={handleSuccessGoogleLogin} />
+            </GoogleOAuthProvider>
+          </div>
+          {/* Footer Link */}
+          <div className="text-center text-[14px] mt-2 space-y-5">
+            <div>
+              <span>Don&apos;t have an account?</span>
+              <button
+                type="button"
+                className="pl-1 text-blue-600 font-[600] hover:underline"
+                onClick={() => router.push("/account/register")}
+              >
+                Sign up
+              </button>
+            </div>
+            <div>
+              Go back to
+              <button
+                type="button"
+                className="pl-1 text-blue-600 font-[600] hover:underline"
+                onClick={() => router.push("/")}
+              >
+                home page
+              </button>
+            </div>
           </div>
         </div>
       </form>
-    </>
+    </div>
   );
 }
--- src/frontend/src/app/(pages)/(auth)/account/login/page.tsx
+++ src/frontend/src/app/(pages)/(auth)/account/login/page.tsx
@@ -4,21 +4,34 @@ import Image from "next/image";
 export default function UserLoginPage() {
   return (
     <>
-      <div className="fixed inset-0 ">
-        <Image
-          src="/bg-account.svg"
-          alt="Hình nền"
-          fill
-          className="object-cover"
-          priority
-          sizes="100vw"
-        />
-      </div>
-
-      <main className="relative z-10 min-h-screen grid place-items-center px-4">
-        <div className="w-full max-w-[500px] rounded-[10px] border border-[#DEDEDE] bg-white px-[20px] py-[50px]">
-          <h1 className="text-center text-[30px] font-[600]">Đăng nhập  </h1>
-          <FormLogin />
+      <main className="min-h-screen flex">
+        <div className="w-2/3 flex items-center justify-center px-4 bg-white">
+          <div className="w-full max-w-[450px]">
+            <div className="mb-[30px] flex text-center flex-col items-center gap-2">
+              <h1 className="text-[40px] font-semibold mb-[20px] text-blue-400">
+                Yuki
+              </h1>
+              <div className="text-[32px] text-gray-600 mb-[3px] line-height-[100%]">
+                <span>Welcome </span>
+                <span className="text-blue-500 font-[600]">back!</span>
+              </div>
+              <p className="text-[12px] text-gray-500 font-semibold">
+                Discover manga, manhua and manhwa, track your progress, have
+                fun, read manga.
+              </p>
+            </div>
+            <FormLogin />
+          </div>
+        </div>
+        <div className="w-1/2 relative hidden lg:block">
+          <Image
+            src="/image/backgroud_account.png"
+            alt="Hình nền"
+            fill
+            className="object-cover"
+            priority
+            sizes="50vw"
+          />
         </div>
       </main>
     </>
