36a1466d45085b59cd7cd4efcf3cd93db7dd9e0c
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2026-03-24 15:31:33 +0700

    connect to reids

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import clientRoutes from "./routes/client/index.route";
 import adminRoutes from "./routes/admin/index.route";
 import * as variableConfig from "./config/variable.config";
-
-dotenv.config();
+import { connectRedis } from "./config/redis.config";
 
 declare global {
   var pathAdmin: string;
@@ -16,6 +16,8 @@ const app = express();
 const port = 5000;
 
 global.pathAdmin = variableConfig.pathAdmin;
+
+// connectRedis();
 
 app.use(
   cors({
@@ -30,6 +32,10 @@ app.use(express.json());
 app.use("/", clientRoutes);
 app.use(`/${pathAdmin}`, adminRoutes);
 
-app.listen(port, () => {
-  console.log(`Your website is running at port: http://localhost:${port}`);
-});
+(async () => {
+  await connectRedis();
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})();
